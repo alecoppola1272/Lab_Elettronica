3a6c6d3381d763405319630052bd27717609f825
Alessandro Coppola <102584241+alecoppola1272@users.noreply.github.com>
2023-12-22 13:41:23 +0100

    Update transistor.C

--- transistor.C
+++ transistor.C
@@ -5,30 +5,39 @@
 #include "TMultiGraph.h"
 #include "TStyle.h"
 #include "TLegend.h"
+#include <iostream>
 #include "TROOT.h"
 #include "TString.h"
 
 void transistor()
 {
-  gStyle->SetOptFit(111);
+  gStyle->SetOptFit(0);
   TCanvas *c = new TCanvas("Caratteristica di Uscita Transistor BJT");
   c->SetGrid();
-  gStyle->SetErrorX(0);
 
   TGraphErrors *g100 = new TGraphErrors("100micro.dat");
-  //g100->SetPointError(); check error X
-  // g100->SetMarkerStyle(2);
-  // g100->SetMarkerColor(kRed);
-  // g100->SetLineColor(kRed);
+  g100->SetLineColor(12);
 
   TGraphErrors *g50 = new TGraphErrors("50micro.dat");
-  // g50->SetMarkerStyle(2);
-  // g50->SetMarkerColor(kBlue);
-  // g50->SetLineColor(kBlue);
+  g50->SetLineColor(12);
 
-  TLegend *leg = new TLegend(.1, .7, .3, .9, "Punti Sperimentali");
-  leg->AddEntry(g100, "I_{C}=-100mA");
-  leg->AddEntry(g50, "I_{C}=-50mA");
+  g100->Fit("pol1", "Q", "", 0.5, 4.5);
+  TF1 *fit100 = g100->GetFunction("pol1");
+  fit100->SetRange(0, 5);
+  fit100->SetParName(1, "g");
+  fit100->SetLineColor(kRed);
+  fit100->SetLineWidth(1);
+
+  g50->Fit("pol1", "Q", "", 0.5, 4.5);
+  TF1 *fit50 = g50->GetFunction("pol1");
+  fit50->SetRange(0, 5);
+  fit50->SetParName(1, "g");
+  fit50->SetLineColor(kBlue);
+  fit50->SetLineWidth(1);
+
+  TLegend *leg = new TLegend(0.65, 0.13, 0.88, 0.28, "Corrente di Base");
+  leg->AddEntry(fit100, "I_{B}=-100 microA");
+  leg->AddEntry(fit50, "I_{B}=-50 microA");
 
   TMultiGraph *g = new TMultiGraph();
   g->SetTitle("Caratteristica di Uscita Transistor;Voltage |V_{CE}| (V);Current |I_{C}| (mA)");
@@ -36,28 +45,14 @@ void transistor()
   g->Add(g50);
 
   g->Draw("APE");
-//controllare parametri
-  TF1 *f100 = new TF1("f100", "[0]+[1]*x", 0.4, 4.2);
-  f100->SetRange(0, 4.2);
-  f100->SetParameter(0,10);
-  f100->SetParName(0, "Tensione di Early");
-  f100->SetParName(1, "Resistance");
-  f100->SetLineColor(kBlue);
-
-  TF1 *f50 = new TF1("f50", "[0]+[1]*x", 0.4, 4.2);
-  f50->SetParameter(0,10);
-  f50->SetRange(0, 4.2);
-  f50->SetParName(0, "Tensione di Early");
-  f50->SetParName(1, "Resistance");
-  f50->SetLineColor(kRed);
-
-  g100->Fit(f100);
-  TF1 *fit100 = g100->GetFunction("f100");
-
-  g50->Fit(f50);
-  TF1 *fit50 = g50->GetFunction("f100");
-
   leg->Draw("SAME");
+  std::cout << "CORRENTE DI BASE I_b = 100 microA" << std::endl;
+  std::cout << "p0 (intercetta) : " << fit100->GetParameter(0) << " +/- " << fit100->GetParError(0) << std::endl;
+  std::cout << "p1 (slope) : " << fit100->GetParameter(0) << " +/- " << fit100->GetParError(0) << std::endl;
+  std::cout << "---------------------------------------------------------------------------------" << std::endl;
+  std::cout << "CORRENTE DI BASE I_b = 50 microA" << std::endl;
+  std::cout << "p0 (intercetta) : " << fit50->GetParameter(0) << " +/- " << fit50->GetParError(0) << std::endl;
+  std::cout << "p1 (slope) : " << fit50->GetParameter(0) << " +/- " << fit50->GetParError(0) << std::endl;
 
   c->Print("transistorBJT.pdf");
 }
